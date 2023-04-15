d60ddb2704eb7c7b2a5c0d3642978845e9126e52
zaimonsays <simon.ramirez.ojeda@gmail.com>
2023-04-15 15:18:45 +0000

    avance 1

---
+++ my-app/src/data.js
@@ -0,0 +1,2 @@
+export const lenguajes = ["PHP", "Java", "Python", "Ruby", "C#", "C++"];
+export const frameworks =["Laravel", "Flask", "Django", "Rails", ".Net", "Spring Boot"];
---
+++ my-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'boostrap/dist/css/boostrap.min.css';
+import 'boostrap/dist/js/boostrap.bundle';
+import './index.css'
+
+
+
+ReactDOM.createRoot(document.getElementById( 'root' )).render(<App />)  
